6c43778f203595f299d9176a7b151d802f03a081
simonperssonnn <persson_simon@outlook.com>
2021-09-14 16:05:47 +0200

    Home på g

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import PosterPage from './PosterPage'
 
 export default function Home() {
+
+    const [setPosterPage] = useState([]);
+
+     useEffect(() => {
+        fetchPoster();
+    }, []) 
+
+    const fetchPosters = async () => {
+        try {
+            const response = await fetch('');
+            if (!response.ok) {
+                throw new Error('HTTP Error! status: ' + response.status);
+            }
+            const data = await response.json();
+            setItems(data);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+
+
     return (
-        <div>
-            <h1>BETALA RÅ!!!</h1>
-            
-        </div>
+    <div>
+
+    </div>
+
     )
 }
