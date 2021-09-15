44f8d0030eb98036b7be73ecf259a2f728c1e4c0
unknown <mxzetterlund@gmail.com>
2021-09-15 10:22:16 +0200

    Added internal CSS

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -4,9 +4,22 @@ import React from 'react'
 import Error404 from '../icons/error404.svg'
 
 
+const errorStyle = { 
+    position: 'fixed',
+    width: 'fit-content',
+    top: '28%',
+    bottom: '25%',
+    margin: 'auto',
+    left: '25%',
+    right: '25%', 
+}
+
+
+
+
 export default function Error() {
     return (
-        <div>
+        <div style={errorStyle}>
             <img src={Error404} alt = 'Error 404'/>
             <h1>Error 404</h1>
             <h2>Page not found</h2>
