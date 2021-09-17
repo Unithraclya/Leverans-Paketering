e4492012e6ab7c7bdaaca589baf10a7fe9210314
simonperssonnn <persson_simon@outlook.com>
2021-09-17 11:40:32 +0200

    Home typ klar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,47 @@
-import React, {useState, useEffect} from 'react'
-import PosterPage from './PosterPage'
+import React from 'react'
+import styles from '../css/Home.module.css'
+import CartIcon from '../icons/cartfilled36dp.svg'
 
 export default function Home() {
-
-    const [setPosterPage] = useState([]);
-
-     useEffect(() => {
-        fetchPoster();
-    }, []) 
-
-    const fetchPosters = async () => {
-        try {
-            const response = await fetch('');
-            if (!response.ok) {
-                throw new Error('HTTP Error! status: ' + response.status);
-            }
-            const data = await response.json();
-            setItems(data);
-            console.log(data);
-        } catch (error) {
-            console.log(error);
-        }
-
-    }
-
-
-
     return (
-    <div>
+        <div className={styles.mainContainer}>
+            <div className={styles.header}>
+                <p>Free shipping over 500kr!</p>
+            </div>
+            <div className={styles.items}>
+                <div className={styles.item}>
+                    <div className={styles.top}>
+                    </div>
+                    <div className={styles.btns}>
+                      {/*  CATEGORY BUTTON  */}
+                    <div className={styles.dropdown}>
+                        <button className={styles.dropbtn}>Category ▼</button>
+                        <div className={styles.dropdownContent}>
+                            <a className={styles.a}href="#">Animals</a>
+                            <a className={styles.a}href="#">Sexy firemen</a>
+                            <a className={styles.a}href="#">City</a>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                    
+                        {/*  PRICE BUTTON  */}
+                        
+                    <div className={styles.dropdown}>
+                        <button className={styles.dropbtn}>Price ▼</button>
+                        <div className={styles.dropdownContent}>
+                            <a className={styles.a}href="#">0KR - 200KR</a>
+                            <a className={styles.a}href="#">200KR - 500KR</a>
+                            </div>
+                        </div>
+                        
+                    
+                                
+           </div>
+           <div className={styles.image}></div>
+        </div>
 
-    </div>
-
+        
     )
+
 }
