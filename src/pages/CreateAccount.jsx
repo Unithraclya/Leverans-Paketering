7c43d8a4c6d6bca39d27de7c5b5a9751066c3217
unknown <mxzetterlund@gmail.com>
2021-09-24 16:32:43 +0200

    new

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -1,40 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
+import { useHistory } from "react-router-dom";
 
-export default async function CreateAccount() {
+//Styles
+import CrAccStyle from '../css/CreateAccount.module.css'
 
+// simplify fetch
 async function simpleFetch(url, settings) {
-    return await (await fetch(url, settings)).json();
-  }
-
-    
-    let users = await simpleFetch('/api/users');
-    // Build html from User data
-    let html = '';
-    
-    for (let { id, name, mail, password } of users) {
-        
-        html += `
-        <tr>
-          <td>${id}</td>
-          <td>${name}</td>
-          <td>${mail}</td>
-          <td>${password}</td>
-        </tr>
-      `;
-    }
-    // Create a table and add the html and add the div to the body
-    let table = document.createElement('table');
-    table.innerHTML = html;
-    document.body.append(table);
-  
-
-      
-    
-   
+  return await (await fetch(url, settings)).json();
 }
 
-async function addUser(name, mail, password) {
-    let result = await simpleFetch('/api/users', {
+export default function CreateAccount() {
+  const [users, setUser] = useState([]);
+  const [name, setName] = useState([]);
+  const [mail, setMail] = useState([]);
+  const [password, setPassword] = useState([]);
+
+
+  // fetch all users
+
+  useEffect(() => {
+    (async () => {
+      //fetch all users
+      setUser(await(await fetch('api/users')).json());
+    })();
+  }, []);
+
+
+  async function addUser(name, mail, password) {
+    let result = await simpleFetch('/api/users:id', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ name, mail, password })
@@ -42,29 +35,46 @@ async function addUser(name, mail, password) {
     return result;
   }
   
-  async function updateUser(id, fieldsToChangeAsObject) {
-    let result = await simpleFetch('/api/users/' + id, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(fieldsToChangeAsObject)
-    });
-    return result;
-  }
-  
-  async function deleteUser(id) {
-    let result = await simpleFetch('/api/users/' + id, {
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' }
-    });
-    return result;
-  }
-  
-  // Make sure I  can test some functions directly from console
-  window.addUser = addUser;
-  window.updateUser = updateUser;
-  window.deleteUser = deleteUser;
-  
-  CreateAccount();
+
+  // A working fetch all users 
+
+  // return (
+  //   <div className="App">
+  //       {users.map(({id, name, mail, password})=> 
+  //           <div key={id}>
+  //           <h3>{name} </h3>
+  //           <p>Email: {mail}</p>
+  //           <p>Birth date: {password}</p>
+  //         </div>
+  //       )}
+  //   </div>
+  // )
+
+
+
+  return (
+    <div className={CrAccStyle.Form}>
+         
+         <form>
+       <input type="text" name={name} placeholder="Namn"/>
+       <input type="text" mail={mail} placeholder="Mail"/>
+       <input type="text" password={password} placeholder="password"/>
+
+          <input type="submit" onClick={addUser} value="Skapa konto" />
+        </form>
+
+         {users.map(({id, name, mail, password})=> 
+            <div key={id}>
+            <h2>{name}</h2>
+            <h3>{mail}</h3>
+            <h3>{password}</h3>
+        
+        </div>
+         )} 
+    </div>
+  )
+}
+
   
 
 
