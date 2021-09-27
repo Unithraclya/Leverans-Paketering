2786ba0892bfbc4d8434e389c06d9ef75a3b25b2
unknown <mxzetterlund@gmail.com>
2021-09-27 14:25:28 +0200

    Update CreateAccount.jsx
    connecting to database

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -4,73 +4,58 @@ import { useHistory } from "react-router-dom";
 //Styles
 import CrAccStyle from '../css/CreateAccount.module.css'
 
-// simplify fetch
-async function simpleFetch(url, settings) {
-  return await (await fetch(url, settings)).json();
-}
 
 export default function CreateAccount() {
-  const [users, setUser] = useState([]);
-  const [name, setName] = useState([]);
-  const [mail, setMail] = useState([]);
-  const [password, setPassword] = useState([]);
 
+  const [name, setNameReg] = useState('');
+  const [mail, setMailReg] = useState('');
+  const [password, setPasswordReg] = useState('');
 
-  // fetch all users
-
-  useEffect(() => {
-    (async () => {
-      //fetch all users
-      setUser(await(await fetch('api/users')).json());
-    })();
-  }, []);
-
-
-  async function addUser(name, mail, password) {
-    let result = await simpleFetch('/api/users:id', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name, mail, password })
-    });
-    return result;
+async function addUser() {
+  let result = await fetch('/api/users', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, mail, password })
   }
-  
-
-  // A working fetch all users 
-
-  // return (
-  //   <div className="App">
-  //       {users.map(({id, name, mail, password})=> 
-  //           <div key={id}>
-  //           <h3>{name} </h3>
-  //           <p>Email: {mail}</p>
-  //           <p>Birth date: {password}</p>
-  //         </div>
-  //       )}
-  //   </div>
-  // )
-
+  );
+  return result;
+}
 
 
   return (
     <div className={CrAccStyle.Form}>
          
-         <form>
-       <input type="text" name={name} placeholder="Namn"/>
-       <input type="text" mail={mail} placeholder="Mail"/>
-       <input type="text" password={password} placeholder="password"/>
+      <form onSubmit={addUser}>
+       <input 
+       type="text" 
+       placeholder="Användarnamn"
+       username={name}
+       onChange={(e) => {
+       setNameReg(e.target.value);
+       }} 
+       />
+       <input 
+       type="text"
+       placeholder="E-post"
+       mail={mail}
+       onChange={(e) => {
+       setMailReg(e.target.value);
+       }}
+       />
+       <input
+       type="text"
+       placeholder="Lösenord"
+       password={password}
+       onChange={(e) => {
+       setPasswordReg(e.target.value);
+       }}
+       />
+        <input 
+        type="submit"
+        placeholder="Skapa konto" 
+        />
+      </form>
 
-          <input type="submit" onClick={addUser} value="Skapa konto" />
-        </form>
-
-         {users.map(({id, name, mail, password})=> 
-            <div key={id}>
-            <h2>{name}</h2>
-            <h3>{mail}</h3>
-            <h3>{password}</h3>
-        
-        </div>
-         )} 
     </div>
   )
 }
